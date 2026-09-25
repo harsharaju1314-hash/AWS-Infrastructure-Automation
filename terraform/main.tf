428997ba5e458f63becc751e9d1d934d213d0de0
Harshavardhan varma <harsharaju1314@gmail.com>
2026-09-25 13:28:21 +0530

    fix(terraform): format HCL files to comply with terraform fmt check

--- terraform/main.tf
+++ terraform/main.tf
@@ -173,12 +173,12 @@ resource "aws_iam_instance_profile" "ec2_profile" {
 # Compute: EC2 Instance
 # ------------------------------------------------------------------------------
 resource "aws_instance" "web" {
-  ami                  = data.aws_ami.ubuntu.id
-  instance_type        = var.instance_type
-  subnet_id            = aws_subnet.public.id
+  ami                    = data.aws_ami.ubuntu.id
+  instance_type          = var.instance_type
+  subnet_id              = aws_subnet.public.id
   vpc_security_group_ids = [aws_security_group.web_sg.id]
-  iam_instance_profile = aws_iam_instance_profile.ec2_profile.name
-  key_name             = var.key_name != "" ? var.key_name : null
+  iam_instance_profile   = aws_iam_instance_profile.ec2_profile.name
+  key_name               = var.key_name != "" ? var.key_name : null
 
   # Encrypted root storage volume
   root_block_device {
